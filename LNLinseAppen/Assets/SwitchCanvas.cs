d7c4a479c9f06adf99d1dc57fe94af6af143310d
Allan Sørensen <aldanv@live.dk>
2018-09-24 09:40:15 +0200

    Actual push

---
+++ LNLinseAppen/Assets/SwitchCanvas.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchCanvas : MonoBehaviour {
+
+    [SerializeField]
+    List<GameObject> canvases;
+    [SerializeField]
+    GameObject currentCanvas;
+
+
+    public void AlarmCanvas()
+    {
+        SwitchToNewCanvas(canvases[0]);
+    }
+
+    public void UserPageCanvas()
+    {
+        SwitchToNewCanvas(canvases[1]);
+    }
+
+    void SwitchToNewCanvas(GameObject newCanvas)
+    {
+        currentCanvas.active = false;
+        newCanvas.active = true;
+        currentCanvas = newCanvas;
+    }
+}
