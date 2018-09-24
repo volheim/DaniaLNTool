b745b22ac59ec3f464a0062019f394ee349aa7fd
Arne <Arne0480@student.eadania.dk>
2018-09-24 10:43:26 +0200

    Login

---
+++ LNLinseAppen/Assets/SwitchScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchScene : MonoBehaviour
+{
+
+    public void ChangeScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+}
