6c4a6457c83ae5cea0d8dec5d2f02f92757aae36
Allan Sørensen <aldanv@live.dk>
2018-09-25 10:24:23 +0200

    Linse sortering styrke

---
+++ LNLinseAppen/Assets/LinseSorteringLinseStyrke.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinseSorteringLinseStyrke : MonoBehaviour {
+
+    [SerializeField]
+    int styrke;
+
+    [SerializeField]
+    bool linse;
+	// Use this for initialization
+	void Start () {
+        if (linse)
+        {
+            styrke = Random.Range(-2, 3);
+        }
+	}
+
+}
---
+++ LNLinseAppen/Assets/LinseSorteringMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinseSorteringMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position = new Vector3(Input.mousePosition.x / 80,0,0);
+	}
+}
