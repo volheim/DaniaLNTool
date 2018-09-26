b736f8dd2fb40f53953e46aaafb6c69bc38c2ffb
Allan Sørensen <aldanv@live.dk>
2018-09-26 12:05:13 +0200

    Movement & initial work on mobile keyboard

--- LNLinseAppen/Assets/LinseSorteringMovement.cs
+++ LNLinseAppen/Assets/LinseSorteringMovement.cs
@@ -4,14 +4,32 @@ using UnityEngine;
 
 public class LinseSorteringMovement : MonoBehaviour {
 
+    Vector3 move;
+
+    [SerializeField]
+    float speed;
+    [SerializeField]
+    int screenWidth;
+
 	// Use this for initialization
 	void Start () {
-		
+        screenWidth = Screen.width;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        transform.position = new Vector3(Input.mousePosition.x / 80,0,0);
-	}
+        move = Input.GetTouch(0).deltaPosition;
+
+        transform.Translate(new Vector3(move.x,0,0)/screenWidth*speed);
+        if (transform.position.x > 7.9f)
+        {
+            transform.position = new Vector3(7.9f,0,0);
+        }
+        if (transform.position.x < -7.9f)
+        {
+            transform.position = new Vector3(-7.9f, 0, 0);
+        }
+
+    }
 }
