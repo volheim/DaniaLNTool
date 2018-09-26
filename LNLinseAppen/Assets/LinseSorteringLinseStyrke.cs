7566bbd2c78fb50b7bdd1faa4e556326b349e70a
Arne <Arne0480@student.eadania.dk>
2018-09-26 12:06:11 +0200

    Point system, Sortering efter farve

--- LNLinseAppen/Assets/LinseSorteringLinseStyrke.cs
+++ LNLinseAppen/Assets/LinseSorteringLinseStyrke.cs
@@ -11,21 +11,27 @@ public class LinseSorteringLinseStyrke : MonoBehaviour
     [SerializeField]
     bool linse;
 
+    [SerializeField]
+    List<Color> colors;
 
-    FallDown fd;
+    // FallDown fd;
 
     // Use this for initialization
     void Start()
     {
-
+        colors.Add(Color.black);
+        colors.Add(Color.blue);
+        colors.Add(Color.yellow);
+        colors.Add(Color.gray);
+        colors.Add(Color.red);
         if (linse)
         {
-            fd = GetComponent<FallDown>();
+          //  fd = GetComponent<FallDown>();
             styrke = Random.Range(-2, 3);
-            gameObject.GetComponent<MeshRenderer>().materials[0] = fd.colors[styrke + 2];
+           // gameObject.GetComponent<MeshRenderer>().materials[0] = fd.colors[styrke + 2];
+
         }
-
-
+        gameObject.GetComponent<MeshRenderer>().material.color = colors[styrke + 2];
     }
 
 }
